a9a5c36cd83c16948d4379a223eeac2eb7d06c08
wangyilei <wangyilei@bytedance.com>
2021-04-25 23:53:52 +0800

    bugfix:moment parse error

--- src/pages/approval/InfoInput.tsx
+++ src/pages/approval/InfoInput.tsx
@@ -36,8 +36,12 @@ export default class InfoInput extends Component<InfoInputProps> {
             case 'text': return <Input.TextArea {...props} value={value} onChange={(val) => onChange?.call(this, val)} />
             case 'time': return <DatePicker {...props} value={moment(value)} onChange={(val) => onChange?.call(this, val.format())} />
             case 'times': {
-                const moments = value.split(",").map(it => moment(it))
-                return <DatePicker.RangePicker {...props} value={[moments[0], moments[1]]} onChange={(val) => onChange?.call(this, val[0] + "," + val[1])} />
+                if (value) {
+                    const moments = value.split(",").map(it => moment(parseInt(it)))
+                    return <DatePicker.RangePicker {...props} value={[moments[0], moments[1]]} onChange={(val) => onChange?.call(this, val[0] + "," + val[1])} />
+                } else {
+                    return <DatePicker.RangePicker {...props} onChange={(val) => onChange?.call(this, val[0] + "," + val[1])} />
+                }
             }
             default: return null
         }
